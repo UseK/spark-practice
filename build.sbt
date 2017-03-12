b5227da8824bf4ba7dd41ae316634639c2dfa57f
UseK <use.k.james@gmail.com>
2017-03-12 19:51:19 +0900

    call job.print() but nothing prints... orz

--- build.sbt
+++ build.sbt
@@ -3,4 +3,7 @@ name := "stockholder_meeting"
 version := "1.0"
 
 scalaVersion := "2.12.1"
-    
+
+libraryDependencies += "org.apache.pdfbox" % "pdfbox" % "2.0.4"
+libraryDependencies += "org.bouncycastle" % "bcprov-jdk15" % "1.44"
+libraryDependencies += "org.bouncycastle" % "bcmail-jdk15" % "1.44"
--- src/main/scala/Hello.scala
+++ src/main/scala/Hello.scala
@@ -1,3 +1,22 @@
+import java.awt.print.PrinterJob
+import java.io.File
+
+import org.apache.pdfbox.pdmodel.PDDocument
+import org.apache.pdfbox.printing.PDFPageable
+import java.awt.print.Book
+import java.awt.print.PageFormat
+import java.awt.print.Paper
+import java.awt.print.PrinterException
+import java.awt.print.PrinterJob
+import java.io.File
+import java.io.IOException
+import javax.print.attribute.HashPrintRequestAttributeSet
+import javax.print.attribute.PrintRequestAttributeSet
+import javax.print.attribute.standard.PageRanges
+import org.apache.pdfbox.pdmodel.PDDocument
+import org.apache.pdfbox.printing.PDFPageable
+import org.apache.pdfbox.printing.PDFPrintable
+
 /**
   * Created by yf on 2017/03/12.
   */
@@ -8,5 +27,20 @@ class Hello {
 object Hello {
   def main(args: Array[String]): Unit = {
     println("hello sbt")
+    val path ="data/pdf/hitachi.pdf"
+    //val f = new File()
+    //print(f.exists())
+    val document = PDDocument.load(new File(path))
+    print(document)
+  }
+
+  def print(document: PDDocument): Unit = {
+    val job = PrinterJob.getPrinterJob()
+    job.setPageable(new PDFPageable(document))
+    //val attr = new HashPrintRequestAttributeSet()
+    //attr.add(new PageRanges(1, 1)) // pages 1 to 1
+    println("<<<")
+    job.print()
+    println(">>>")
   }
 }
