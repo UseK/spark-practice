c2bdaed8595e1e07f03314658490f7d09140d1bd
UseK <use.k.james@gmail.com>
2017-04-04 23:56:04 +0900

    Succeed to show links in first result of search

--- src/main/scala/Crawler.scala
+++ src/main/scala/Crawler.scala
@@ -6,12 +6,18 @@ import net.ruippeixotog.scalascraper.browser.JsoupBrowser
 class Crawler {
   val browser = JsoupBrowser()
   val google_search_uri = "https://www.google.co.jp/search?q=%s"
+  val URIPattern = """(http://.+?)&.+$""".r
 
-  def get_pdf_links(companyName: String): List[String] = {
-    val doc = browser.get(google_search_uri.format(companyName))
-    val links = doc.root.select("h3>a").headOption.fold("")(_.text.trim)
-    print(links)
-    List("aiee")
+  def getSearchResults(companyName: String): Iterable[String] = {
+    val uri = google_search_uri.format(companyName)
+    getLinks(uri, "h3>a")
   }
 
+  def getLinks(uri: String, selector: String) = {
+    val doc = browser.get(uri)
+    val rawLinks = doc.root.select(selector).map(_.attr("href"))
+    println(rawLinks)
+    rawLinks.flatMap(URIPattern.findFirstMatchIn)
+      .map(_.group(1))
+  }
 }
--- src/test/scala/TestCrawler.scala
+++ src/test/scala/TestCrawler.scala
@@ -9,7 +9,9 @@ class TestCrawler extends FunSpec {
       val crawler = new Crawler()
       val expectedStr = "https://www.google.co.jp/search?q=日立 株主総会"
       assert(expectedStr == crawler.google_search_uri.format("日立 株主総会"))
-      crawler.get_pdf_links("日立 株主総会")
+      val firstResult = crawler.getSearchResults("日立 株主総会").head
+      val links = crawler.getLinks(firstResult, "a[href]")
+      println(links)
     }
   }
 }
