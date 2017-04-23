5ff8d1626394d721fd19170a5e406ba21782c3e0
UseK <use.k.james@gmail.com>
2017-04-23 10:28:28 +0900

    Add TestSpark, now just fail

---
+++ src/test/scala/TestSpark.scala
@@ -0,0 +1,14 @@
+import org.scalatest.FunSpec
+
+/**
+  * Created by yf on 2017/04/23.
+  */
+class TestSpark extends FunSpec {
+  describe("Spark") {
+    it("works") {
+      assert(1 == 2)
+      println("aiee")
+
+    }
+  }
+}
