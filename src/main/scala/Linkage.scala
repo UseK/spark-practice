0a17ddc679c0354ca80f268261e2617c45cde844
UseK <use.k.james@gmail.com>
2017-04-26 21:39:01 +0900

    Linkage.parse return MatchData(id1: Int, id2: Int, scores: Array[Double], matched: Boolean)

--- src/main/scala/Linkage.scala
+++ src/main/scala/Linkage.scala
@@ -11,10 +11,18 @@ object Linkage {
     if (s == "?") Double.NaN else s.toDouble
   }
 
-  def parse(line: String) = {
-    println(line)
+  case class MatchData(id1: Int,
+                       id2: Int,
+                       scores: Array[Double],
+                       matched: Boolean)
+
+  def parse(line: String): MatchData = {
+    println(line) //36950,42116,1,?,1,1,1,1,1,1,1,TRUE
     val pieces = line.split(",")
     val id1 = pieces(0).toInt
+    val id2 = pieces(1).toInt
     val scores = pieces.slice(2, pieces.length-1).map(toDouble)
+    val matched = pieces(pieces.length-1) == "TRUE"
+    MatchData(id1, id2, scores, matched)
   }
 }
--- src/test/scala/TestSpark.scala
+++ src/test/scala/TestSpark.scala
@@ -7,10 +7,11 @@ import org.scalatest.FunSpec
   */
 class TestSpark extends FunSpec {
   describe("Spark") {
+    val sc = new SparkContext("local", "example")
+    val rawBlocks = sc.textFile("data/linkage")
+    val head = rawBlocks.take(10)
+
     it("works") {
-      val sc = new SparkContext("local", "example")
-      val rawBlocks = sc.textFile("data/linkage")
-      val head = rawBlocks.take(10)
       val expected =
         """"id_1","id_2","cmp_fname_c1",
           |"cmp_fname_c2","cmp_lname_c1",
@@ -21,20 +22,19 @@ class TestSpark extends FunSpec {
       assert(expected == head(0))
       assert(head(0) == rawBlocks.take(10)(0))
       assert(LK.isHeader(head(0)))
+    }
+
+    it("parse") {
+      val matchData = LK.parse(head(5))
+      assert(matchData.id1 == 36950)
+      assert(matchData.id2 == 42116)
+      assert(matchData.scores(0) == 1.0)
+      assert(matchData.matched == true)
+    }
+
+    it("bodies") {
       val bodies = head.filter(!LK.isHeader(_))
       assert(9 == bodies.length)
-      LK.parse(head(5))
-      class AieeInt  {
-        def aiee = "aiee"
-      }
-      implicit def int2AieeInt(x: Int): AieeInt = new AieeInt
-      implicit def int2String(x: Int): String = "aiee"
-      implicit def arraryStiring2Int(x: Array[String]): Int = {
-        114514
-      }
-      println(1 + Array("1"))
-      val one = 1
-      println(one.aiee)
     }
   }
 }
