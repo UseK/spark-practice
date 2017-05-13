03dd966dece26f5f709e171b508f4020583910f6
UseK <use.k.james@gmail.com>
2017-05-13 17:07:02 +0900

    Don't show log4j INFO

--- src/main/scala/RunSpark.scala
+++ src/main/scala/RunSpark.scala
@@ -1,4 +1,6 @@
-import com.usek.stockfoldermeeting.{Linkage => LK, MySpark}
+import com.usek.stockfoldermeeting.{MySpark, Linkage => LK}
+import org.apache.hadoop.yarn.util.RackResolver
+import org.apache.log4j.{Level, Logger}
 
 /**
   * Created by yf on 2017/04/30.
@@ -7,6 +9,11 @@ object RunSpark {
 
 
   def main(args: Array[String]): Unit = {
+    Logger.getLogger(classOf[RackResolver]).getLevel
+    Logger.getLogger("org").setLevel(Level.OFF)
+    Logger.getLogger("akka").setLevel(Level.OFF)
+    System.setProperty("log4j.warn", "")
+
     val mySpark = MySpark("data/linkage")
     val mds = mySpark.head.filter(LK.isBody).map(LK.parse)
     val parsed = mySpark.noHeader.map(LK.parse)
